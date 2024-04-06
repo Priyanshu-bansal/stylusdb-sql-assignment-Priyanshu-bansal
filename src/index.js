6dbaf2c318b76a96ff09ed9dfc65d3e8395f2a77
Priyanshu <97589300+Priyanshu-bansal@users.noreply.github.com>
2024-04-06 16:19:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-const parseQuery = require('./queryParser');
+const {parseQuery, parseQueryWhere} = require('./queryParser');
 const readCSV = require('./csvReader');
 
 async function executeSELECTQuery(query) {
@@ -16,4 +16,27 @@ async function executeSELECTQuery(query) {
     });
 }
 
-module.exports = executeSELECTQuery
+
+async function executeWhereQuery(query) {
+    const { fields, table, whereClause } = parseQueryWhere(query);
+    const data = await readCSV(`${table}.csv`);
+    
+    // Filtering based on WHERE clause
+    const filteredData = whereClause
+        ? data.filter(row => {
+            const [field, value] = whereClause.split('=').map(s => s.trim());
+            return row[field] === value;
+        })
+        : data;
+
+    // Selecting the specified fields
+    return filteredData.map(row => {
+        const selectedRow = {};
+        fields.forEach(field => {
+            selectedRow[field] = row[field];
+        });
+        return selectedRow;
+    });
+}
+
+module.exports = {executeSELECTQuery, executeWhereQuery}
