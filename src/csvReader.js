479ecade85830188e5e73b3797a862bb9395e1cc
Priyanshu <97589300+Priyanshu-bansal@users.noreply.github.com>
2024-04-06 16:09:35 +0530

    Update csvReader.js

--- src/csvReader.js
+++ src/csvReader.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const fs = require('fs');
 const csv = require('csv-parser');
 
@@ -19,25 +19,4 @@ function readCSV(filePath) {
 }
 
 module.exports = readCSV;
-=======
-const fs = require('fs');
-const csv = require('csv-parser');
 
-function readCSV(filePath) {
-    const results = [];
-
-    return new Promise((resolve, reject) => {
-        fs.createReadStream(filePath)
-            .pipe(csv())
-            .on('data', (data) => results.push(data))
-            .on('end', () => {
-                resolve(results);
-            })
-            .on('error', (error) => {
-                reject(error);
-            });
-    });
-}
-
-module.exports = readCSV;
->>>>>>> 8a2387bd8cea8a310eabf43259684fab483d9e09
